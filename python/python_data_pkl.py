61bb734325f78c642951b144591ec9b8abb99252
Letian-Wang <wangletian1995@126.com>
2020-06-12 23:57:34 -0700

    reorganize

--- python/python_data_pkl.py
+++ python/python_data_pkl.py
@@ -3,3 +3,7 @@ import scipy.io
 import pickle
 a=pickle.load( open( 'xy_list.pkl', "rb" ) )
 scipy.io.savemat('xy_list2.mat', mdict={'pickle_data': a})
+
+# Load data from pkl
+with open('INTERACTION_Dataset/xy_list.pkl', 'rb') as f:
+    data = pickle.load(f)
